474bb8825047f574778139afb19ac92501d65256
COUILLET Dylan <36613221+Nanotekfr@users.noreply.github.com>
2019-09-25 19:46:11 +0200

    Updates

---
+++ precache-manifest.98b7ac787ce2d0647400e9ac76d7d271.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96545b335891f3c79df67435dd020c07",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "470519695a0a44ab4462",
+    "url": "/todo/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "41c20e82fe9c349975c2",
+    "url": "/todo/static/js/2.9eebc151.chunk.js"
+  },
+  {
+    "revision": "470519695a0a44ab4462",
+    "url": "/todo/static/js/main.eb6c15ff.chunk.js"
+  },
+  {
+    "revision": "774cd892e4241f644cd4",
+    "url": "/todo/static/js/runtime-main.7247f3e7.js"
+  }
+]);
